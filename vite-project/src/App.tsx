910a99a796db5209ece99ef79e1b7a6397252f1e
TLRKiliann <philogenie@protonmail.com>
2024-02-05 08:21:51 +0100

    colors changed for the frame

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -43,13 +43,13 @@ function App() {
 
   const colorsQuoteText = [
       "linear-gradient(30deg, rgba(255, 69, 0, 1), rgba(255, 165, 0, 1))",
-      "linear-gradient(30deg, rgba(255, 255, 0, 1), rgba(0, 255, 255, 1))",
-      "linear-gradient(30deg, rgba(144, 238, 144, 1), rgba(0, 255, 255, 1))",
-      "linear-gradient(30deg, rgba(0, 255, 255, 1), rgba(238, 130, 238, 1))"
+      "linear-gradient(30deg, rgba(255, 69, 0, 1), rgba(255, 255, 0, 1))",
+      "linear-gradient(30deg, rgba(255, 165, 0, 1), rgba(255, 255, 0, 1))",
+      "linear-gradient(30deg, rgba(255, 165, 0, 1), rgba(238, 130, 238, 1))"
   ];
 
   const colorsQuoteText2 = [
-    "linear-gradient(30deg, rgba(255, 69, 0, 1), rgba(255, 165, 0, 1))",
+    "linear-gradient(30deg, rgba(144, 238, 144, 1), rgba(255, 255, 0, 1))",
     "linear-gradient(30deg, rgba(255, 255, 0, 1), rgba(0, 255, 255, 1))",
     "linear-gradient(30deg, rgba(144, 238, 144, 1), rgba(0, 255, 255, 1))",
     "linear-gradient(30deg, rgba(0, 255, 255, 1), rgba(238, 130, 238, 1))"
