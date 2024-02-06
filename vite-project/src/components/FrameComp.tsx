faa7f2a2d7ecca8b6b88303d1955888c273a7691
TLRKiliann <philogenie@protonmail.com>
2024-02-06 16:01:18 +0100

    chackra-ui installed for spinner loader

--- vite-project/src/components/FrameComp.tsx
+++ vite-project/src/components/FrameComp.tsx
@@ -1,30 +1,41 @@
+import { Spinner } from '@chakra-ui/spinner'
+//import urlImg from "/images/palmer.jpg";
 import './styles/FrameComp.css';
 
 type FrameProps = {
     colorBgOne: string;
     colorBgTwo: string;
     urlImg: string;
-    data: {content: string}
+    data: {content: string};
+    isLoading: boolean;
 }
 
-export default function FrameComp({ colorBgOne, colorBgTwo, urlImg, data }: FrameProps) {
-    return (
-        <div className="frame">
+export default function FrameComp({ colorBgOne, colorBgTwo, urlImg, data, isLoading }: FrameProps) {
 
-            <div className="para-quote" 
-              style={{background: `linear-gradient(30deg, ${colorBgOne}, ${colorBgTwo}`}}
-            >
-              {urlImg && data.content ? (
-                <div className="para-img">
-                  <p className="layer-quote">{data?.content}</p>
-                  <img src={urlImg} width={800} height={450} alt="img" className="img" />
-                </div>
-                ) : data?.content ? data.content : urlImg ? (
-                  <img src={urlImg} width={800} height={450} alt="img" className="img" />
-                ) : null
-              }
-            </div>
-          
-        </div>
-    )
+  if (isLoading) {
+    return (
+      <div style={{position: "absolute", top: "0px", right: "0px", bottom: "0px", left: "0px"}}>
+        <Spinner />
+      </div>
+  )};
+
+  return (
+    <div className="frame">
+
+      <div className="para-quote" 
+        style={{background: `linear-gradient(30deg, ${colorBgOne}, ${colorBgTwo}`}}
+      >
+        {urlImg && data.content ? (
+          <div className="para-img">
+            <p className="layer-quote">{data?.content}</p>
+            <img src={urlImg} width={800} height={450} alt="picsum img " className="img" />
+          </div>
+          ) : data?.content ? data.content : urlImg ? (
+            <img src={urlImg} width={800} height={450} alt="picsum img " className="img" />
+          ) : null
+        }
+      </div>
+      
+    </div>
+  )
 }
