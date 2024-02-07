56fc8b4895b8237e48847fa24fc19a46260f8015
TLRKiliann <philogenie@protonmail.com>
2024-02-07 04:33:55 +0100

    new type definitions added

--- vite-project/src/lib/definitions.ts
+++ vite-project/src/lib/definitions.ts
@@ -2,3 +2,13 @@ export type PhotoProps = {
     id: number;
     name: string;
 }
+
+export type DataProps = {
+    content: string;
+};
+  
+export type HollyBoolyProps = {
+    changeQuote: boolean;
+    changeImg: boolean;
+    boolBoxImg: boolean;
+};
