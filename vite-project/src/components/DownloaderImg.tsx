b54d0bf5f7fb27220d0a5e01a99690e75d53c99a
TLRKiliann <philogenie@protonmail.com>
2024-02-08 18:33:51 +0100

    div added to improved style with bgColor gradient

--- vite-project/src/components/DownloaderImg.tsx
+++ vite-project/src/components/DownloaderImg.tsx
@@ -1,30 +1,35 @@
 import type { PhotoProps } from "../lib/definitions";
+import './styles/DownloaderImg.css';
 
 type DownloaderImgProps = {
-    hollyBooly: {
-        boolBoxImg: boolean;
-    }
-    handleBoolBoxImg: () => void;
-    photos: {
-        id: number;
-        name: string;
-    }[]
-    handleDownloadImg: (id: number) => void; 
+  colorBgOne: string;
+  colorBgTwo: string;
+  hollyBooly: {
+      boolBoxImg: boolean;
+  }
+  handleBoolBoxImg: () => void;
+  photos: {
+      id: number;
+      name: string;
+  }[]
+  handleDownloadImg: (id: number) => void; 
 }
 
-export default function DownloaderImg({ hollyBooly, handleBoolBoxImg, photos, handleDownloadImg }: DownloaderImgProps) {
+export default function DownloaderImg({ colorBgOne, colorBgTwo, hollyBooly, handleBoolBoxImg, photos, handleDownloadImg }: DownloaderImgProps) {
     return (
         <>
             {hollyBooly.boolBoxImg === true ? (
               <div className="container-img">
-                <div className="box-img">
+                <div className="box-img" style={{background: `linear-gradient(30deg, ${colorBgOne}, ${colorBgTwo})`}}>
 
-                  <span onClick={handleBoolBoxImg} className="close-span">X</span>
-                  <h4>Download an image: </h4>
-              
-                  {photos.map((photo: PhotoProps) => (
-                    <span key={photo.id} onClick={() => handleDownloadImg(photo.id)}>➡️ {photo.name}</span>
-                  ))}
+                  <div className="box-inner">
+                    <span onClick={handleBoolBoxImg} className="close-span">X</span>
+                    <h4>Choose an image :</h4>
+                
+                    {photos.map((photo: PhotoProps) => (
+                      <span key={photo.id} onClick={() => handleDownloadImg(photo.id)}>➡️ &nbsp;Image: {photo.name}</span>
+                    ))}
+                  </div>
 
                 </div>
               </div>
@@ -33,7 +38,7 @@ export default function DownloaderImg({ hollyBooly, handleBoolBoxImg, photos, ha
 
             <div>
               <button type="button" onClick={handleBoolBoxImg}>
-                Open img box
+                Download
               </button>
             </div>
         </>
